3e52d92c9e3ec289bf35251220bb9a2a5a9a256c
k023g0096@g.neec.ac.jp <k023g0096@g.neec.ac.jp>
2025-01-16 16:45:24 +0900

    導入

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -3,18 +3,44 @@
 
 using namespace KamataEngine;
 
+/// <summary>
+/// ゲームシーン
+/// </summary>
 class GameScene {
-public:
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
 	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~GameScene();
 
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
 	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
 	void Draw();
 
-private:
+private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+	///
 };
