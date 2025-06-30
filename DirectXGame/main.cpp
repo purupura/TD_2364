529ef755c714a0198956f89c1771908bbd856718
k023g0096@g.neec.ac.jp <k023g0096@g.neec.ac.jp>
2025-06-30 23:04:32 +0900

    a

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -16,7 +16,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"2364_しゅわしゅわソーダ");
+	win->CreateGameWindow(L"しゅわしゅわソーダ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
